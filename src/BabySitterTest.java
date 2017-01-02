7ca776714c8b92ec04ffaf3b56bf056a99f0f2c8
Benaiah <elkariond@gmail.com>
2017-01-02 16:55:34 -0500

    Added test for endTime to keep it within time restraints

--- src/BabySitterTest.java
+++ src/BabySitterTest.java
@@ -1,13 +1,25 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BabySitterTest {
 
-	@Test
-	public void WhenBabysitterIsPassedAStartTimeItsAfterStartTime() {
-		Babysitter babysitter = new Babysitter();
-		assertEquals("You entered a good start time!", babysitter.pay(2, "AM"));
+	Babysitter babysitter;
+	
+	@Before
+	public void setUp(){
+		babysitter = new Babysitter();
 	}
-
+	
+	@Test
+	public void WhenBabysitterIsPassedAStartTimeItsAtOrAfterFivePM() {
+		assertEquals("You entered a good start time!", babysitter.pay(2, "AM", 4, "AM"));
+	}
+	
+	@Test
+	public void WhenBabysitterIsPassedAnEndTimeItEndsAtOrBeforeFourAM() {
+		assertEquals("You entered a good start time!", babysitter.pay(7, "PM", 12, "PM"));
+	}
+	
 }
